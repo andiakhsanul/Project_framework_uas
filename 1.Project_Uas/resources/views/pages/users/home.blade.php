700755ac42e16b82bf5ec6dcee6d442711d5c698
andi akhsanul <116082109+andiakhsanul@users.noreply.github.com>
2023-07-10 13:13:03 +0700

    Update home.blade.php

--- 1.Project_Uas/resources/views/pages/users/home.blade.php
+++ 1.Project_Uas/resources/views/pages/users/home.blade.php
@@ -1,20 +1,6 @@
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-<html>
-<h1>berhasil login
 
-</h1>
-
-</html>
-=======
->>>>>>> Stashed changes
 @extends('layout.appUser')
 
 @section('container')
 <h1>konten home</h1>
 @endsection
-<<<<<<< Updated upstream
-=======
->>>>>>> a99626423cdfda8697831f45a51ac76fed46e5b1
->>>>>>> Stashed changes
