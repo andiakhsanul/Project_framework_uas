df9b4526208120485da81f2b245abab3bf8d298e
Ghandi | Phantom <134348061+GhandiZero0X@users.noreply.github.com>
2023-07-14 00:54:11 +0700

    test

--- 1.Project_Uas/routes/web.php
+++ 1.Project_Uas/routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [LoginController::class, 'index'])->name('index');
 
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::post('/submitLogin', [LoginController::class, 'submitLogin'])->name('submitLogin');
 
