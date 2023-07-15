0d1c7fe42248f95db1e09c8593b8627ee326622a
andi akhsanul <116082109+andiakhsanul@users.noreply.github.com>
2023-07-16 00:11:51 +0700

    Update web.php

--- 1.Project_Uas/routes/web.php
+++ 1.Project_Uas/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CatatanController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\NotifikasiController;
 use App\Http\Controllers\RegisterController;
