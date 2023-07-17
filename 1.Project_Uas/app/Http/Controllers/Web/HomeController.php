a908a2eca0979f43ec77d9fafca15043a6a0a781
Ghandi | Phantom <134348061+GhandiZero0X@users.noreply.github.com>
2023-07-17 23:15:39 +0700

    [+] Update
    ganti logic

--- 1.Project_Uas/app/Http/Controllers/Web/HomeController.php
+++ 1.Project_Uas/app/Http/Controllers/Web/HomeController.php
@@ -15,13 +15,11 @@ class HomeController extends Controller
             $namaUser = Mahasiswa::where('NAMA', $user->NAMA)->value('NAMA');
 
             $jadwalharian = $user->catatans; // Retrieve the jadwalharian records associated with the user
-            $activeFormId = null; // Inisialisasi variabel $activeFormId
 
             return view('pages.users.home', [
                 'title' => 'Home',
                 'namaUser' => $namaUser,
                 'jadwalharian' => $jadwalharian, // Pass the jadwalharian variable to the view
-                'activeFormId' => $activeFormId // Pass the activeFormId variable to the view
             ]);
         } else {
             // Pengguna tidak terotentikasi, lakukan penanganan yang sesuai
